df67d467cce0d8eea4eaaff40d8d8d359e38cc5a
Renegatto <edittorr@mail.ru>
2020-06-10 00:50:42 +0300

    beginning...

--- Program.fs
+++ Program.fs
@@ -1,5 +1,17 @@
 ﻿// Learn more about F# at https://fsharp.org
 // See the 'F# Tutorial' project for more help.
+type BlockData = {
+    block_id : int
+}
+type UnboundPoint = {
+    relative : int*int*int 
+    data : BlockData
+}
+type Point = {
+    absolute : int*int*int
+    relative : int*int*int
+    data : BlockData
+}
 
 [<EntryPoint>]
 let main argv =
